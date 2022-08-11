c0973149c59f27514e7b8245cf3714e0f5b8bcc9
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-10 21:35:18 -0400

    fniished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // require your server and launch it
+
+/*
+    npm install
+*/
+
+
+const server = require("./api/server.js")
+
+const port = 8000;
+
+server.listen(port, () => {
+    console.log("Server running on: ", port)
+})
